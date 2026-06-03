c0a5f8e5d6f5925a060db57dd24e17e7302f201f
Maksim <6818069@stud.op.edu.ua>
2026-06-03 03:45:09 +0300

    poprav

--- app/src/main/java/com/example/expenses/MainActivity.kt
+++ app/src/main/java/com/example/expenses/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.expenses.ui.ExpenseScreen
-import com.example.expenses.ui.theme.ExpensesDayTheme // Перевір назву своєї теми (якщо підкреслить червоним, просто видали цей імпорт і натисни Alt+Enter)
+import com.example.expenses.ui.theme.ExpensesDayTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ExpenseScreen() // Наш головний екран витрат
+                    ExpenseScreen()
                 }
             }
         }
